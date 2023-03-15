54825eebf223cd797673a9a6347f89e7c4df96c7
zrdouane <ramirdouane@gmail.com>
2023-03-15 14:24:32 +0100

    confetti_cuisine

---
+++ contentTypes.js
@@ -0,0 +1,20 @@
+module.exports = {
+  html: {
+    "Content-Type": "text/html"
+  },
+  text: {
+    "Content-Type": "text/plain"
+  },
+  js: {
+    "Content-Type": "text/js"
+  },
+  jpg: {
+    "Content-Type": "image/jpg"
+  },
+  png: {
+    "Content-Type": "image/png"
+  },
+  css: {
+    "Content-Type": "text/css"
+  }
+};
